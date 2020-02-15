ddeec0b0b213317607a484a66408ea26294e8441
Okina-Shimpei <okinashinpei@gmail.com>
2020-02-15 19:28:29 +0900

    Chapter2 ASP.NET MVCの基本 P57

--- MvcBasic/MvcBasic/Controllers/BeginController.cs
+++ MvcBasic/MvcBasic/Controllers/BeginController.cs
@@ -1,9 +1,11 @@
-﻿using System.Web.Mvc;
+﻿using MvcBasic.Models;
+using System.Web.Mvc;
 
 namespace MvcBasic.Controllers
 {
     public class BeginController : Controller
     {
+        private MvcBasicContext db = new MvcBasicContext();
         // GET: Begin
         public ActionResult Index()
         {
@@ -14,6 +16,9 @@ namespace MvcBasic.Controllers
             ViewBag.Message = " こんにちは、世界！ ";
             return View();
         }
-
+        public ActionResult List()
+        {
+            return View(db.Members);
+        }
     }
 }
