0a3328ac617fd00c9cfdbfc9c9d76be7caf4603e
Okina-Shimpei <okinashinpei@gmail.com>
2020-02-24 18:08:14 +0900

    Chapter4 ビュー開発 P132

---
+++ ViewTraining/ViewTraining/Helpers/MyHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewTraining.Helpers
+{
+    public static class MyHelper
+    {
+        public static IHtmlString Mailto(String address, String linktext)
+        {
+            return MvcHtmlString.Create(
+                String.Format("<a href=\"mailto:{0}\">{1}</a>",
+                HttpUtility.HtmlAttributeEncode(address),
+                HttpUtility.HtmlAttributeEncode(linktext)));
+        }
+    }
+}
